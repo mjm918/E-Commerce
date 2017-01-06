9468d1311ea2099cb6858a24431fa7294db483d9
mohammad <jxmxshakil@gmail.com>
2017-01-06 10:12:25 +0800

    PHP version is now changed to php-cgi7.0.1

--- index.php
+++ index.php
@@ -8,9 +8,16 @@
 ?>
 <!DOCTYPE html>
     <head>
-
+        <!--For Mobile rendering-->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
     <body>
-
     </body>
+<!--Link for project css-->
+<link rel="stylesheet" type="text/css" href="css/style.css"/>
+<!--Link for material design css and js-->
+<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+<link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
+<script defer src="https://code.getmdl.io/1.3.0/material.min.js" />
+
 </html>
